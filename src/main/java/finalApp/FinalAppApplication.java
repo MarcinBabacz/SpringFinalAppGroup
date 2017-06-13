fbffeb622df1385ebfd452d860f72dd61f929178
Marcin <babacz.marcin@gmail.com>
2017-06-13 12:08:39 +0200

    Add app

---
+++ src/main/java/finalApp/FinalAppApplication.java
@@ -0,0 +1,12 @@
+package finalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalAppApplication.class, args);
+	}
+}
